db54fcdac5a8714625dfc3228e68ad1f1d36a9a8
Marcus <78104059+SlyFryFrog@users.noreply.github.com>
2024-08-11 01:43:26 -0400

    Update CMake to add imgui

--- LilyPadEngine/src/LilyPad/core/math/vector2i.hpp
+++ LilyPadEngine/src/LilyPad/core/math/vector2i.hpp
@@ -4,11 +4,11 @@
 
 namespace LilyPad
 {
-    struct Vector2i
-    {
-        int x;
-        int y;
+	struct Vector2i
+	{
+		int x;
+		int y;
 
 		_FORCE_INLINE_ bool operator<=>(const Vector2i &other) const = default;
-    };
+	};
 } // namespace LilyPad
